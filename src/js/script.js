de162a7caf14723a8a8774cc5004f98f3f466b03
Alvaro <alvarof260@gmail.com>
2023-03-17 12:43:35 -0300

    fixed array

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,19 @@
 //array de productos
 const productos = [
-    {id:1, nombre:'Fear of God Essentials Pullover Hoodie Applique Logo', image:'fearlogo.jpg', descuento:false, precio:36120, tipo: 'buzo'}
-    {id:2, nombre:'Sp5der Angel Number Hoodie', image:'sp5der.jpg', descuento:false, precio:61000, tipo: 'buzo'}
-    {id:3, nombre:'Saint Mxxxxxx Angel Hoodie', image:'saintmxxx.jpg', descuento:false, precio:100000, tipo: 'buzo'}
-    {id:4, nombre:'Nike x Stussy Crew Fleece', image:'nikexstussy.jpg', descuento:false, precio:36850, tipo: 'buzo'}
-    {id:5, nombre:'Juice Wrld x Vlone 999 T-shirt', image:'vlone99.jpg', descuento:false, precio:15200, tipo: 'remera'}
-    {id:6, nombre:'Anti Social Social Club Cancelled T-shirt', image:'antisocial.jpg', descuento:false, precio:14900, tipo: 'remera'}
-    {id:7, nombre:'Supreme T-Rex Tee', image:'supremeTrex.jpg', descuento:false, precio:14.400, tipo: 'remera'}
-    {id:8, nombre:'Pop Smoke x Vlone The Woo T-shirt', image:'popsmoke.jpg', descuento:false, precio:20250, tipo: 'remera'}
-    {id:9, nombre:'Nike Tech Fleece Joggers', image:'niketech.jpg', descuento:false, precio:19300, tipo: 'pantalon'}
-    {id:10, nombre:'KAWS x The North Face Youth Sweatpant', image:'kaws.jpg', descuento:false, precio:29300, tipo: 'pantalon'}
-    {id:11, nombre:'Stone Island Pantalone Regular Tapered Pants', image:'stoneIsland.jpg', descuento:false, precio:83950, tipo: 'pantalon'}
-    {id:12, nombre:'LOEWE Women\'s Anagram Pocket Tapered Denim Jeans', image:'loewe.jpg', descuento:false, precio:128350, tipo: 'pantalon'}
-    {id:13, nombre:'adidas Yeezy 700 V3', image:'adidasyezzy.jpg', descuento:false, precio:72000, tipo: 'zapatillas'}
-    {id:14, nombre:'Jordan 4 Retro', image:'joran4retro.jpg', descuento:false, precio:39000, tipo: 'zapatillas'}
+    {id:1, nombre:'Fear of God Essentials Pullover Hoodie Applique Logo', image:'fearlogo.jpg', descuento:false, precio:36120, tipo: 'buzo'},
+    {id:2, nombre:'Sp5der Angel Number Hoodie', image:'sp5der.jpg', descuento:false, precio:61000, tipo: 'buzo'},
+    {id:3, nombre:'Saint Mxxxxxx Angel Hoodie', image:'saintmxxx.jpg', descuento:false, precio:100000, tipo: 'buzo'},
+    {id:4, nombre:'Nike x Stussy Crew Fleece', image:'nikexstussy.jpg', descuento:false, precio:36850, tipo: 'buzo'},
+    {id:5, nombre:'Juice Wrld x Vlone 999 T-shirt', image:'vlone99.jpg', descuento:false, precio:15200, tipo: 'remera'},
+    {id:6, nombre:'Anti Social Social Club Cancelled T-shirt', image:'antisocial.jpg', descuento:false, precio:14900, tipo: 'remera'},
+    {id:7, nombre:'Supreme T-Rex Tee', image:'supremeTrex.jpg', descuento:false, precio:14.400, tipo: 'remera'},
+    {id:8, nombre:'Pop Smoke x Vlone The Woo T-shirt', image:'popsmoke.jpg', descuento:false, precio:20250, tipo: 'remera'},
+    {id:9, nombre:'Nike Tech Fleece Joggers', image:'niketech.jpg', descuento:false, precio:19300, tipo: 'pantalon'},
+    {id:10, nombre:'KAWS x The North Face Youth Sweatpant', image:'kaws.jpg', descuento:false, precio:29300, tipo: 'pantalon'},
+    {id:11, nombre:'Stone Island Pantalone Regular Tapered Pants', image:'stoneIsland.jpg', descuento:false, precio:83950, tipo: 'pantalon'},
+    {id:12, nombre:'LOEWE Women\'s Anagram Pocket Tapered Denim Jeans', image:'loewe.jpg', descuento:false, precio:128350, tipo: 'pantalon'},
+    {id:13, nombre:'adidas Yeezy 700 V3', image:'adidasyezzy.jpg', descuento:false, precio:72000, tipo: 'zapatillas'},
+    {id:14, nombre:'Jordan 4 Retro', image:'joran4retro.jpg', descuento:false, precio:39000, tipo: 'zapatillas'},
     {id:15, nombre:'Nike Dunk High Retro', image:'nikedunk.jpg', descuento:false, precio:25900, tipo: 'zapatillas'}
 ]
 //mostrar array de productos
