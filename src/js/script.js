7ea23a18c55e3ab851f60f17a726b9ee6eb98f9f
Alvaro <alvarof260@gmail.com>
2023-03-17 12:42:18 -0300

    version 1.1 add array of objects

--- src/js/script.js
+++ src/js/script.js
@@ -1,51 +1,24 @@
-// primer algoritmo
-let consulta = preguntar()
-let precioZapas = 30000
-let precioRemera = 10000
-let precioPantalon = 20000
-let precioBuzo = 14000
-let carrito = 0
-
-while (consulta != 'cancelar') {
-    switch (consulta) {
-        case 'zapatilla':
-            carrito = sumaPrecioFinal(precioZapas,carrito)
-            mensajeProductos(precioZapas,carrito)
-            consulta = preguntar()
-            break;
-        case 'remera':
-            carrito = sumaPrecioFinal(precioRemera,carrito)
-            mensajeProductos(precioRemera,carrito)
-            consulta = preguntar()
-            break;
-        case 'pantalon':
-            carrito = sumaPrecioFinal(precioPantalon,carrito)
-            mensajeProductos(precioPantalon,carrito)
-            consulta = preguntar()
-            break;
-        case 'buzo':
-            carrito = sumaPrecioFinal(precioBuzo,carrito)
-            mensajeProductos(precioBuzo,carrito)
-            consulta = preguntar()
-            break;
-        default:
-            alert('no existe o no hay stock de ese producto')
-            consulta = preguntar()
-            break;
-    }
-}
-
-
-function mensajeProductos(precio,compraFinal) {
-    alert('precio de zapatillas es de: ' + precio + ' Coste final es de:' + compraFinal)
-}
-
-function preguntar() {
-    let consulta = prompt('Ingrese el nombre del producto que quiere comprar(escribir cancelar si no quieres mas) 1-zapatilla,2-remera,3-pantalon,4-buzo:').toLowerCase()
-    return consulta
-}
-
-function sumaPrecioFinal(precio,carrito) {
-    carrito += precio
-    return carrito
-}
+//array de productos
+const productos = [
+    {id:1, nombre:'Fear of God Essentials Pullover Hoodie Applique Logo', image:'fearlogo.jpg', descuento:false, precio:36120, tipo: 'buzo'}
+    {id:2, nombre:'Sp5der Angel Number Hoodie', image:'sp5der.jpg', descuento:false, precio:61000, tipo: 'buzo'}
+    {id:3, nombre:'Saint Mxxxxxx Angel Hoodie', image:'saintmxxx.jpg', descuento:false, precio:100000, tipo: 'buzo'}
+    {id:4, nombre:'Nike x Stussy Crew Fleece', image:'nikexstussy.jpg', descuento:false, precio:36850, tipo: 'buzo'}
+    {id:5, nombre:'Juice Wrld x Vlone 999 T-shirt', image:'vlone99.jpg', descuento:false, precio:15200, tipo: 'remera'}
+    {id:6, nombre:'Anti Social Social Club Cancelled T-shirt', image:'antisocial.jpg', descuento:false, precio:14900, tipo: 'remera'}
+    {id:7, nombre:'Supreme T-Rex Tee', image:'supremeTrex.jpg', descuento:false, precio:14.400, tipo: 'remera'}
+    {id:8, nombre:'Pop Smoke x Vlone The Woo T-shirt', image:'popsmoke.jpg', descuento:false, precio:20250, tipo: 'remera'}
+    {id:9, nombre:'Nike Tech Fleece Joggers', image:'niketech.jpg', descuento:false, precio:19300, tipo: 'pantalon'}
+    {id:10, nombre:'KAWS x The North Face Youth Sweatpant', image:'kaws.jpg', descuento:false, precio:29300, tipo: 'pantalon'}
+    {id:11, nombre:'Stone Island Pantalone Regular Tapered Pants', image:'stoneIsland.jpg', descuento:false, precio:83950, tipo: 'pantalon'}
+    {id:12, nombre:'LOEWE Women\'s Anagram Pocket Tapered Denim Jeans', image:'loewe.jpg', descuento:false, precio:128350, tipo: 'pantalon'}
+    {id:13, nombre:'adidas Yeezy 700 V3', image:'adidasyezzy.jpg', descuento:false, precio:72000, tipo: 'zapatillas'}
+    {id:14, nombre:'Jordan 4 Retro', image:'joran4retro.jpg', descuento:false, precio:39000, tipo: 'zapatillas'}
+    {id:15, nombre:'Nike Dunk High Retro', image:'nikedunk.jpg', descuento:false, precio:25900, tipo: 'zapatillas'}
+]
+//mostrar array de productos
+//cargar al carrito
+//eliminar cantidad de un productos
+//aumentar elemento del carrito
+//vaciar al carrito
+//validacion del pago
