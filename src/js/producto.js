738bb110353b93802ec922e47d3003716ea9ad1b
Alvaro <alvarof260@gmail.com>
2023-03-23 10:15:42 -0300

    falta codigo

---
+++ src/js/producto.js
@@ -0,0 +1,10 @@
+//funcion Constructora de los productos
+function Producto(id, nombre, imagen, descuento, precio, tipo, stock){
+    this.id = id
+    this.nombre = nombre
+    this.imagen = imagen
+    this.descuento = descuento
+    this.precio = precio
+    this.tipo = tipo
+    this.stock = stock
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,37 @@
-//array de productos
-const productos = [
-    {id:1, nombre:'Fear of God Essentials Pullover Hoodie Applique Logo', image:'fearlogo.jpg', descuento:false, precio:36120, tipo: 'buzo'},
-    {id:2, nombre:'Sp5der Angel Number Hoodie', image:'sp5der.jpg', descuento:false, precio:61000, tipo: 'buzo'},
-    {id:3, nombre:'Saint Mxxxxxx Angel Hoodie', image:'saintmxxx.jpg', descuento:false, precio:100000, tipo: 'buzo'},
-    {id:4, nombre:'Nike x Stussy Crew Fleece', image:'nikexstussy.jpg', descuento:false, precio:36850, tipo: 'buzo'},
-    {id:5, nombre:'Juice Wrld x Vlone 999 T-shirt', image:'vlone99.jpg', descuento:false, precio:15200, tipo: 'remera'},
-    {id:6, nombre:'Anti Social Social Club Cancelled T-shirt', image:'antisocial.jpg', descuento:false, precio:14900, tipo: 'remera'},
-    {id:7, nombre:'Supreme T-Rex Tee', image:'supremeTrex.jpg', descuento:false, precio:14.400, tipo: 'remera'},
-    {id:8, nombre:'Pop Smoke x Vlone The Woo T-shirt', image:'popsmoke.jpg', descuento:false, precio:20250, tipo: 'remera'},
-    {id:9, nombre:'Nike Tech Fleece Joggers', image:'niketech.jpg', descuento:false, precio:19300, tipo: 'pantalon'},
-    {id:10, nombre:'KAWS x The North Face Youth Sweatpant', image:'kaws.jpg', descuento:false, precio:29300, tipo: 'pantalon'},
-    {id:11, nombre:'Stone Island Pantalone Regular Tapered Pants', image:'stoneIsland.jpg', descuento:false, precio:83950, tipo: 'pantalon'},
-    {id:12, nombre:'LOEWE Women\'s Anagram Pocket Tapered Denim Jeans', image:'loewe.jpg', descuento:false, precio:128350, tipo: 'pantalon'},
-    {id:13, nombre:'adidas Yeezy 700 V3', image:'adidasyezzy.jpg', descuento:false, precio:72000, tipo: 'zapatillas'},
-    {id:14, nombre:'Jordan 4 Retro', image:'joran4retro.jpg', descuento:false, precio:39000, tipo: 'zapatillas'},
-    {id:15, nombre:'Nike Dunk High Retro', image:'nikedunk.jpg', descuento:false, precio:25900, tipo: 'zapatillas'}
+//catalogo
+const productosArray = [
+    new Producto(1, 'Fear of God Essentials Pullover Hoodie Applique Logo', 'fearlogo.jpg', false, 36120, 'buzo',35),
+    new Producto(2, 'Sp5der Angel Number Hoodie', 'sp5der.jpg', false, 61000, 'buzo',10),
+    new Producto(3, 'Saint Mxxxxxx Angel Hoodie', 'saintmxxx.jpg', false, 100000, 'buzo',5),
+    new Producto(4, 'Nike x Stussy Crew Fleece', 'nikexstussy.jpg', false, 36850, 'buzo',25),
+    new Producto(5, 'Juice Wrld x Vlone 999 T-shirt', 'vlone99.jpg', false, 15200, 'remera',70),
+    new Producto(6, 'Anti Social Social Club Cancelled T-shirt', 'antisocial.jpg', false, 14900, 'remera',125),
+    new Producto(7, 'Supreme T-Rex Tee', 'supremeTrex.jpg', false, 14.400, 'remera',200),
+    new Producto(8, 'Pop Smoke x Vlone The Woo T-shirt', 'popsmoke.jpg', false, 20250, 'remera',400),
+    new Producto(9, 'Nike Tech Fleece Joggers', 'niketech.jpg', false, 19300, 'pantalon',150),
+    new Producto(10, 'KAWS x The North Face Youth Sweatpant', 'kaws.jpg', false, 29300, 'pantalon',18),
+    new Producto(11, 'Stone Island Pantalone Regular Tapered Pants', 'stoneIsland.jpg', false, 83950, 'pantalon',7),
+    new Producto(12, 'LOEWE Women\'s Anagram Pocket Tapered Denim Jeans', 'loewe.jpg', false, 128350, 'pantalon',5),
+    new Producto(13, 'adidas Yeezy 700 V3', 'adidasyezzy.jpg', false, 72000, 'zapatillas',10),
+    new Producto(14, 'Jordan 4 Retro', 'joran4retro.jpg', false, 39000, 'zapatillas',4),
+    new Producto(15, 'Nike Dunk High Retro', 'nikedunk.jpg', false, 25900, 'zapatillas',10)
 ]
-//mostrar array de productos
+
+let catalogo = 'Elegi los productos segun su numero:\n0 - dejar de comprar\n'
+productosArray.forEach(producto => {
+    catalogo += `${producto.id} - ${producto.nombre}\n`
+})
+let productoBuscado = Number(prompt(catalogo))
+let productoEncontrado = productosArray.find(producto=> producto.id === productoBuscado)
+const carrito = []
+carrito.push(productoEncontrado)
+let mostrarCarrito = carrito.map(producto=>{
+    return precio: 
+})
+console.log(productoEncontrado)
+console.log(carrito)
+
+//mostrar lista de productos
 //cargar al carrito
 //eliminar cantidad de un productos
 //aumentar elemento del carrito
